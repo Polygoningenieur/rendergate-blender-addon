02bb160c23f7541e1b7b62e46fa8051509a8f061
Roy Ammerschuber <roy@ammerschuber.de>
2025-10-04 15:40:45 +0200

    switch to production

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ bl_info = {
     "author": "Polygoningenieur Gustav Hahn",
     "description": "Allows you to render in the cloud with Rendergate.ch",
     "blender": (4, 5, 0),
-    "version": (0, 1, 68),
+    "version": (0, 2, 0),
     "location": "Properties -> Render",
     "warning": "",
     "doc_url": "https://github.com/Polygoningenieur/rendergate-blender-addon",
--- config.py
+++ config.py
@@ -5,11 +5,10 @@ import logging
 
 
 REGION: str = "us-east-2"
-USER_POOL_ID: str = "us-east-2_0iJztlRUB"
-USER_POOL_WEB_CLIENT_ID: str = "6m7eldka3q9f20nmev7smovnf6"
+USER_POOL_ID: str = "us-east-2_2BByuLtIX"
+USER_POOL_WEB_CLIENT_ID: str = "5pafa8521fg1gl8opp8p908d9p"
 URL: str ="https://rendergate.ch"
-RENDERGATE_API:str ="https://vhvr3fdsg5.execute-api.us-east-2.amazonaws.com/default"
+RENDERGATE_API:str ="https://5qa62yq9vi.execute-api.us-east-2.amazonaws.com/default"
 LOGGING_LEVEL = logging.ERROR
-
 JOB_REFRESH_RATE=30.0 # seconds
 DOWNLOAD_REFRESH_RATE=30.0 # seconds
